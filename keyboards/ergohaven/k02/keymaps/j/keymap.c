d74bf54547341f6e99bcd0a99175f2362a17f849
kissetfall <j@ergohaven.xyz>
2023-01-16 21:01:59 +0300

    k02 layers

--- keyboards/ergohaven/k02/keymaps/j/keymap.c
+++ keyboards/ergohaven/k02/keymaps/j/keymap.c
@@ -6,6 +6,19 @@ char wpm_str[4];
 #define _LOWER 1
 #define _RAISE 2
 #define _ADJUST 3
+#define _FOUR 4
+#define _FIVE 5
+#define _SIX 6
+#define _SEVEN 7
+#define _EIGHT 8
+#define _NINE 9
+#define _TEN 10
+#define _ELEVEN 11
+#define _TWELVE 12
+#define _THIRTEEN 13
+#define _FOURTEEN 14
+#define _FIFTEEN 15
+#define _SIXTEEN 16
 
 #define RAISE   MO(_RAISE)
 #define LOWER   MO(_LOWER)
@@ -98,6 +111,45 @@ static void print_status_narrow(void) {
             break;
         case _ADJUST:
             oled_write_P(PSTR("Adj\n"), false);
+            break;
+        case _FOUR:
+            oled_write_P(PSTR("Four\n"), false);
+            break;
+        case _FIVE:
+            oled_write_P(PSTR("Five\n"), false);
+            break;
+        case _SIX:
+            oled_write_P(PSTR("Six\n"), false);
+            break;
+        case _SEVEN:
+            oled_write_P(PSTR("Seven"), false);
+            break;
+        case _EIGHT:
+            oled_write_P(PSTR("Eight"), false);
+            break;
+        case _NINE:
+            oled_write_P(PSTR("Nine\n"), false);
+            break;
+         case _TEN:
+            oled_write_P(PSTR("Ten\n"), false);
+            break;
+         case _ELEVEN:
+            oled_write_P(PSTR("Elevn"), false);
+            break;
+         case _TWELVE:
+            oled_write_P(PSTR("Twlve"), false);
+            break;
+         case _THIRTEEN:
+            oled_write_P(PSTR("Thrtn"), false);
+            break;
+         case _FOURTEEN:
+            oled_write_P(PSTR("Frtn\n"), false);
+            break;
+         case _FIFTEEN:
+            oled_write_P(PSTR("Fiftn"), false);
+            break;
+         case _SIXTEEN:
+            oled_write_P(PSTR("Sixtn"), false);
             break;
         default:
             oled_write_ln_P(PSTR("Undef"), false);
