9fa4847e904072880eb52b666de0cd7cc2344508
Danil Tolkachev <dandur.oak@gmail.com>
2024-06-19 23:10:24 +0300

    update trackball

--- keyboards/ergohaven/trackball/config.h
+++ keyboards/ergohaven/trackball/config.h
@@ -4,6 +4,12 @@
 #define VIAL_UNLOCK_COMBO_ROWS { 1, 1 }
 #define VIAL_UNLOCK_COMBO_COLS { 1, 2 }
 
+/* SPI config for display/touchpad*/
+#define SPI_DRIVER SPID1
+#define SPI_SCK_PIN GP10
+#define SPI_MOSI_PIN GP11
+#define SPI_MISO_PIN GP12
+
 #define ADNS9800_CS_PIN GP9
 
 //#define ADNS9800_SPI_MODE 1
