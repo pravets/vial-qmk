436074cb6b1cec903fffb7fe58742c44299c8b41
Danil Tolkachev <dandur.oak@gmail.com>
2024-06-19 18:34:37 +0300

    remove display light pin

--- keyboards/ergohaven/macropad/rev2/display.c
+++ keyboards/ergohaven/macropad/rev2/display.c
@@ -158,9 +158,6 @@ bool display_init_kb(void) {
     display_enabled = false;
     dprint("display_init_kb - start\n");
 
-    gpio_set_pin_output(GP18);
-    gpio_write_pin_high(GP18);
-
     display = qp_st7789_make_spi_device(240, 280, LCD_CS_PIN, LCD_DC_PIN, LCD_RST_PIN, 16, 3);
     qp_set_viewport_offsets(display, 0, 20);
 
@@ -237,11 +234,9 @@ void display_housekeeping_task(void) {
     }
 
     if (last_input_activity_elapsed() > EH_TIMEOUT ) {
-        gpio_write_pin_low(GP18);
         qp_power(display, false);
         return;
     } else {
-        gpio_write_pin_high(GP18);
         qp_power(display, true);
     }
 
