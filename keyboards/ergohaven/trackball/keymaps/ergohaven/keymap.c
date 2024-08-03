510a4ab4bb981e04ea14d22d9df163adfd87e168
Danil Tolkachev <dandur.oak@gmail.com>
2024-08-03 16:02:11 +0300

    simplify code

--- keyboards/ergohaven/trackball/keymaps/ergohaven/keymap.c
+++ keyboards/ergohaven/trackball/keymaps/ergohaven/keymap.c
@@ -3,15 +3,6 @@
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [0] = LAYOUT(
-        KC_MS_BTN1, KC_MS_BTN2
+        KC_NO
   ),
 };
-
-#ifdef ENCODER_MAP_ENABLE
-const uint16_t PROGMEM encoder_map[][NUM_ENCODERS][NUM_DIRECTIONS] = {
-  [0] = { ENCODER_CCW_CW(KC_VOLD, KC_VOLU) },
-  [1] = { ENCODER_CCW_CW(KC_PGDN, KC_PGUP) },
-  [2] = { ENCODER_CCW_CW(KC_WH_D, KC_WH_U) },
-  [3] = { ENCODER_CCW_CW(KC_VOLD, KC_VOLU) },
-};
-#endif
